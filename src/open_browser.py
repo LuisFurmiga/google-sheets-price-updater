ff005ff6dc1a659eb362ff2d208086b36654a5ae
Luis Fernando <40693896+LuisFurmiga@users.noreply.github.com>
2025-09-06 22:44:06 -0300

    Update open_browser.py
    Atualização do open_chrome, pois, estava com um comando que foi removido

--- src/open_browser.py
+++ src/open_browser.py
@@ -6,11 +6,16 @@ def open_chrome():
     # Comando para abrir o Chrome no modo depuração
     chrome_cmd = f'"{CHROME_PATH}" --remote-debugging-port={DEBUG_PORT} --headless=new --disable-gpu --user-data-dir="{CHROME_PROFILE_PATH}"'
     # Inicia o Chrome
-    chrome_process = subprocess.Popen(chrome_cmd, shell=True)
+    chrome_process = subprocess.Popen(
+        chrome_cmd, 
+        shell=True, 
+        stdout=subprocess.DEVNULL, 
+        stderr=subprocess.DEVNULL
+    )
     return chrome_process
 
 # Retorna o processo para poder encerrá-lo depois
 def close_chrome(chrome_process):
     print("Encerrando o Chrome iniciado pelo script...")
     chrome_process.terminate()  # Mata o processo iniciado pelo script
-    os.system('taskkill /F /IM chrome.exe /T')
+    #os.system('taskkill /F /IM chrome.exe /T')
